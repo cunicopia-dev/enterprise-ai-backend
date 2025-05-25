11dc17c4a89b349c3eba692602e0391cedd63ac3
cunicopia <112027862+cunicopia-dev@users.noreply.github.com>
2025-05-25 15:19:34 -0400

    Update database.py

--- src/utils/database.py
+++ src/utils/database.py
@@ -2,8 +2,7 @@
 Database connection and utility module.
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from .config import config
 
