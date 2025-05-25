823bb74a54bbcb35fb117f55eb0751ae51aa1d5a
cunicopia <112027862+cunicopia-dev@users.noreply.github.com>
2025-05-25 15:19:00 -0400

    Update docker-init.sh

--- sql/docker-init.sh
+++ sql/docker-init.sh
@@ -6,6 +6,8 @@ echo "Running PostgreSQL initialization script..."
 # Run the SQL scripts in the correct order
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /app/sql/01_schema.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /app/sql/02_seed_data.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /app/sql/03_multi_provider_schema.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /app/sql/04_seed_providers.sql
 
 # Grant privileges
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
